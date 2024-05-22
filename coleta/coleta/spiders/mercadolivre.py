6805eb2b16a0d22f0206b7ba43fd55541f601a86
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2024-05-22 20:45:14 -0300

    feat: Arquivos do projeto scrapy

---
+++ coleta/coleta/spiders/mercadolivre.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class MercadolivreSpider(scrapy.Spider):
+    name = "mercadolivre"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = ["https://lista.mercadolivre.com.br/celulares-telefones"]
+
+    def parse(self, response):
+        pass
