24186cc53c934e64deea3b79ce773d5954594b67
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2024-05-28 20:46:53 -0300

    feat: Atualização dash app.py

--- SRC/dashboard/app.py
+++ SRC/dashboard/app.py
@@ -12,5 +12,18 @@ df = pd.read_sql_query("SELECT * FROM ML", conn)
 # Fechar a conexão com o banco de dados
 conn.close()
 
+# Configuração CSS
+tabs_font_css = """
+<style>
+    div[class*=stTitle] label{
+        font-size: 26px;
+        color: blue
+}
+</style>
+
+
+"""
+st.markdown(tabs_font_css, unsafe_allow_html=True)
+
 # Título da aplicação
-st.title('Pesquisa de Mercado - Smartphones no Mercado Livre')
+st.title("Pesquisa de Mercado - :blue[_Smartphones no Mercado Livre_]")
