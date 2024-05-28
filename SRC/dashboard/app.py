1908a4f77a70f051314cc3ff0db66694bc83ee8c
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2024-05-28 19:05:23 -0300

    feat: Atualização arquivo app.py

---
+++ SRC/dashboard/app.py
@@ -0,0 +1,16 @@
+# Bibliotecas utilizadas
+import streamlit as st
+import pandas as pd
+import sqlite3
+
+# Conexão com o banco de dados
+conn = sqlite3.connect('../../data/data.db')
+
+# Consulta ao banco de dados para obter os dados tabela 'ML'
+df = pd.read_sql_query("SELECT * FROM ML", conn)
+
+# Fechar a conexão com o banco de dados
+conn.close()
+
+# Título da aplicação
+st.title('Pesquisa de Mercado - Smartphones no Mercado Livre')
